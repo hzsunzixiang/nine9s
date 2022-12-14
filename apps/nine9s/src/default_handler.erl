591a4bb378f399eba6eb69e1cec6d29df4595a30
haichengsun <haichengsun123@163.com>
2022-12-13 21:52:12 -0500

    换行

--- apps/nine9s/src/default_handler.erl
+++ apps/nine9s/src/default_handler.erl
@@ -4,5 +4,5 @@
 init(Req, Opts) ->
     Req2 = cowboy_req:reply(200, [ {<<"content-type">>, 
                                     <<"text/plain">>} ], 
-                            <<"Hello world!">>, Req), 
+                            <<"Hello world!\n">>, Req), 
     {ok, Req2, Opts}.
